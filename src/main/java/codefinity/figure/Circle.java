1b19f6bae96d1a0d1df976eef2c442fb80416dbc
Daniel Potts <djpotts2891@gmail.com>
2024-06-04 15:36:16 +0100

    Final Comit - Task Complete

--- src/main/java/codefinity/figure/Circle.java
+++ src/main/java/codefinity/figure/Circle.java
@@ -9,4 +9,13 @@ public class Circle extends Shape {
         this.radius = radius;
     }
 
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
 }
